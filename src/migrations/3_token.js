cd50080f86a8ca0b3632b52db5873f107636f93c
DNT <ducconit@gmail.com>
2024-02-28 10:19:25 +0700

    chore: update dynamic token

--- src/migrations/3_token.js
+++ src/migrations/3_token.js
@@ -2,7 +2,11 @@ const Token = artifacts.require("Token");
 
 module.exports = function (deployer, network, accounts) {
   deployer.then(async function () {
-    await deployer.deploy(Token, "FAKE", "FAKE");
+    await deployer.deploy(
+      Token,
+      process.env.TOKEN_NAME,
+      process.env.TOKEN_SYMBOL
+    );
     const conToken = await Token.deployed();
     console.log(("Token", conToken.address));
   });
